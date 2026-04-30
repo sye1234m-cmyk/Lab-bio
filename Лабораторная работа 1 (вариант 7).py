7eaac5cf6a16d0a4d05eacf6b83eb8f2577a4ed7
sye1234m-cmyk <sye1234m@gmail.com>
2026-04-30 09:51:48 +0300

    Add files via upload
    Лабораторная работа №1. Вариант 7

---
+++ Лабораторная работа 1 (вариант 7).py
@@ -0,0 +1,39 @@
+#Емельяненко Анастасия
+#Вариант 7. Первый закон Менделя
+
+k, m, n = 2, 2, 2 #сюда вводим три натуральных числа
+total = k + m + n
+recessive = 0.0
+if n >= 2:
+    recessive += (n / total) * ((n - 1) / (total - 1))
+if m >= 1 and n >= 1:
+    recessive += (m / total) * (n / (total - 1)) * 0.5
+    recessive += (n / total) * (m / (total - 1)) * 0.5
+if m >= 2:
+    recessive += (m / total) * ((m - 1) / (total - 1)) * 0.25
+dominant = 1 - recessive
+a= round(dominant, 5)
+print(f"Входные данные: {k} {m} {n}")
+print(f"Вероятность доминантного фенотипа: {a}")
+
+with open('mendel_first_law.py', 'w', encoding='utf-8') as file:
+    file.write('''# Емельяненко Анастасия
+# Вариант 7. Первый закон Менделя
+
+k, m, n = 2, 2, 2
+total = k + m + n
+recessive = 0.0
+
+if n >= 2:
+    recessive += (n / total) * ((n - 1) / (total - 1))
+if m >= 1 and n >= 1:
+    recessive += (m / total) * (n / (total - 1)) * 0.5
+    recessive += (n / total) * (m / (total - 1)) * 0.5
+if m >= 2:
+    recessive += (m / total) * ((m - 1) / (total - 1)) * 0.25
+
+dominant = 1 - recessive
+a = round(dominant, 5)
+print(f"Входные данные: {k} {m} {n}")
+print(f"Вероятность доминантного фенотипа: {a}")
+''')
